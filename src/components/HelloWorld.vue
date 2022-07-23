e943d9d740f38cb6d040ecdadd5d281127d6cc34
rinto <ririnto@kakao.com>
2022-07-23 17:54:32 +0900

    L3-make

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,8 +15,9 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
         >
+          babel
+        </a>
       </li>
       <li>
         <a
@@ -104,11 +105,11 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -116,14 +117,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
